71bf67685bb69be4944da1c29ddb07391aa73cd1
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-03-01 08:22:32 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Dumpify;
+
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<int> collection = [1, 2, 3, 4, 5];
+
+        var even = collection.Where(i => i % 2 == 0);
+        var fooInt = collection.OfType<int>();
+        var fooSkip3 = collection.Skip(3);
+        var fooTakeLast3 = collection.TakeLast(3);
+        var fooLast3 = collection.Take(3);
+        var fooSkipLast3 = collection.SkipLast(3);
+
+        fooSkip3.Dump();
+    }
+}
